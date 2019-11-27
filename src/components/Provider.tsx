d2f82d8be4c577dd46e25e66090212afae8d01ea
Slugalisk <slugalisk@gmail.com>
2019-11-27 03:22:57 -0800

    correct Client type import

--- src/components/Provider.tsx
+++ src/components/Provider.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { BrowserRouter } from "react-router-dom";
-import App, { API } from "../components/App";
+import App from "../components/App";
+import Client from "../service/client";
 
 import "../main.scss";
 
@@ -8,7 +9,7 @@ const { Suspense } = React;
 
 const LoadingMessage = () => (<p className="loading_message">loading</p>);
 
-const Provider = ({client}: {client: API}) => (
+const Provider = ({client}: {client: Client}) => (
   <BrowserRouter>
     <Suspense fallback={<LoadingMessage />}>
       <App client={client} />
