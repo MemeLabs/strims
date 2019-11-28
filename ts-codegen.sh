b6ceb3514cbdbd7960e406b3557d939d36dee832
jbpratt78 <jbpratt78@gmail.com>
2019-11-28 07:59:10 -0600

    update code gen script with some awk magic
    worked with HoppenR to get down to a single, simple awk command
    
    I feel a bit dumb now
    
    Co-authored-by: Christoffer Lundell <christofferlundell@protonmail.com>

--- ts-codegen.sh
+++ ts-codegen.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
-echo "// GENERATED CODE -- DO NOT EDIT!" > "src/service/types.ts"
-echo "import * as api_pb from \"./api_pb\";" >> "src/service/types.ts"
-echo "import { registerType } from \"./rpc_host\";" >> "src/service/types.ts"
-echo "" >> "src/service/types.ts"
+echo "// GENERATED CODE -- DO NOT EDIT!" >"src/service/types.ts"
+echo "import * as api_pb from \"./api_pb\";" >>"src/service/types.ts"
+echo "import { registerType } from \"./rpc_host\";" >>"src/service/types.ts"
+echo "" >>"src/service/types.ts"
 
-sed 's/^message //; s/{}//; /[};]$/d; s/{//' schema/api.proto |
-	awk '/./{ printf "registerType(\"%s\", api_pb.%s)\n", $1, $1}' >> "src/service/types.ts"
+awk '/message/ { { printf "registerType(\"%s\", api_pb.%s)\n", $2, $2}; }' \
+	schema/api.proto >>"src/service/types.ts"
