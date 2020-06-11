dcb08c7d15f327c0398c9b7f2b2dd943f72ab700
jbpratt78 <jbpratt78@gmail.com>
2020-06-10 19:07:38 -0500

    binmap unit testing
    Commented out tests are due to missing a function. There are two tests
    failing which may be bugs.
    
    === RUN   TestInitGet
    --- PASS: TestInitGet (0.00s)
    === RUN   TestNavigation
    --- PASS: TestNavigation (0.00s)
    === RUN   TestOverflows
    --- PASS: TestOverflows (0.00s)
    === RUN   TestAdvanced
    --- PASS: TestAdvanced (0.00s)
    === RUN   TestBinToString
    --- PASS: TestBinToString (0.00s)
    === RUN   TestSetGet
    --- PASS: TestSetGet (0.00s)
    === RUN   TestChess
    --- PASS: TestChess (0.00s)
    === RUN   TestStaircase
    --- PASS: TestStaircase (0.00s)
    === RUN   TestHole
    --- PASS: TestHole (0.00s)
    === RUN   TestFind
        TestFind: map_test.go:87:
            	Error Trace:	map_test.go:87
            	Error:      	Not equal:
            	            	expected: 0x4
            	            	actual  : 0x0
            	Test:       	TestFind
    --- FAIL: TestFind (0.00s)
    === RUN   TestCover
    --- PASS: TestCover (0.00s)
    === RUN   TestSeqLength
        TestSeqLength: map_test.go:143:
            	Error Trace:	map_test.go:143
            	Error:      	Not equal:
            	            	expected: 11
            	            	actual  : 0
            	Test:       	TestSeqLength
    --- FAIL: TestSeqLength (0.00s)
    === RUN   TestEmptyFilled
    --- PASS: TestEmptyFilled (0.00s)
    === RUN   TestFindFilled1
    --- PASS: TestFindFilled1 (0.00s)
    === RUN   TestFindFilled2
    --- PASS: TestFindFilled2 (0.00s)
    === RUN   TestFindFilled3
    --- PASS: TestFindFilled3 (0.00s)
    FAIL
    coverage: 55.1% of statements
    exit status 1
    FAIL	github.com/MemeLabs/go-ppspp/pkg/binmap	0.004s

---
+++ pkg/binmap/bin_test.go
@@ -0,0 +1,56 @@
+package binmap
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestInitGet(t *testing.T) {
+	assert := assert.New(t)
+
+	assert.Equal(0x1, int(NewBin(1, 0)))
+	assert.Equal(0xB, int(NewBin(2, 1)))
+	assert.Equal(0x2, NewBin(2, 1).Layer())
+	assert.Equal(34, NewBin(34, 2345).Layer())
+	assert.Equal(0x7ffffffff, int(NewBin(34, 2345).LayerBits()))
+	assert.Equal(1, NewBin(2, 1).LayerOffset())
+	assert.Equal(2345, NewBin(34, 2345).LayerOffset())
+	assert.Equal((1<<1)-1, int(NewBin(0, 123).LayerBits()))
+	assert.Equal((1<<17)-1, int(NewBin(16, 123).LayerBits()))
+}
+
+func TestNavigation(t *testing.T) {
+	assert := assert.New(t)
+	mid := NewBin(4, 18)
+	assert.Equal(NewBin(5, 9), mid.Parent())
+	assert.Equal(NewBin(3, 36), mid.Left())
+	assert.Equal(NewBin(3, 37), mid.Right())
+	assert.Equal(NewBin(5, 9), NewBin(4, 19).Parent())
+	up32 := NewBin(30, 1)
+	assert.Equal(NewBin(31, 0), up32.Parent())
+}
+
+func TestOverflows(t *testing.T) {
+	assert := assert.New(t)
+	assert.False(None.Contains(NewBin(0, 1)))
+	assert.True(All.Contains(NewBin(0, 1)))
+	assert.Equal(uint64(0), None.BaseLength())
+}
+
+func TestAdvanced(t *testing.T) {
+	assert := assert.New(t)
+	assert.Equal(uint64(4), NewBin(2, 3).BaseLength())
+	assert.False(NewBin(1, 1234).Base())
+	assert.True(NewBin(0, 12345).Base())
+	assert.Equal(NewBin(0, 2), NewBin(1, 1).BaseLeft())
+	assert.Equal(NewBin(0, 3), NewBin(1, 1).BaseRight())
+}
+
+func TestBinToString(t *testing.T) {
+	assert := assert.New(t)
+
+	assert.Equal(All.String(), "ALL")
+	assert.Equal(None.String(), "NONE")
+	assert.Equal(NewBin(0, 1).String(), "2")
+}
---
+++ pkg/binmap/map_test.go
@@ -0,0 +1,217 @@
+package binmap
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSetGet(t *testing.T) {
+	assert := assert.New(t)
+	bs := New()
+	b3 := NewBin(1, 0)
+	b2 := NewBin(0, 1)
+	bs.Set(b3)
+
+	assert.True(bs.FilledAt(b3))
+	assert.True(bs.FilledAt(b2))
+}
+
+func TestChess(t *testing.T) {
+	assert := assert.New(t)
+	chess16 := New()
+
+	for i := 0; i < 16; i++ {
+		if i&1 == 1 {
+			chess16.Set(NewBin(0, uint64(i)))
+		} else {
+			chess16.Reset(NewBin(0, uint64(i)))
+		}
+	}
+
+	for i := 0; i < 16; i++ {
+		if i&1 == 1 {
+			assert.True(chess16.FilledAt(NewBin(0, uint64(i))))
+		} else {
+			assert.True(chess16.EmptyAt(NewBin(0, uint64(i))))
+		}
+	}
+
+	assert.False(chess16.EmptyAt(NewBin(4, 0)))
+	for i := 0; i < 16; i += 2 {
+		chess16.Set(NewBin(0, uint64(i)))
+	}
+
+	assert.True(chess16.FilledAt(NewBin(4, 0)))
+	assert.True(chess16.FilledAt(NewBin(2, 3)))
+
+	chess16.Set(NewBin(4, 1))
+	assert.True(chess16.FilledAt(NewBin(5, 0)))
+}
+
+func TestStaircase(t *testing.T) {
+	assert := assert.New(t)
+	const TOPLAYR = 44
+	staircase := New()
+	for i := 0; i < TOPLAYR; i++ {
+		staircase.Set(NewBin(uint64(i), 1))
+	}
+
+	assert.False(staircase.FilledAt(NewBin(TOPLAYR, 0)))
+	assert.False(staircase.EmptyAt(NewBin(TOPLAYR, 0)))
+
+	staircase.Set(NewBin(0, 0))
+	assert.True(staircase.FilledAt(NewBin(TOPLAYR, 0)))
+}
+
+func TestHole(t *testing.T) {
+	assert := assert.New(t)
+	hole := New()
+	hole.Set(NewBin(8, 0))
+	hole.Reset(NewBin(6, 1))
+	hole.Reset(NewBin(6, 2))
+
+	assert.True(hole.FilledAt(NewBin(6, 0)))
+	assert.True(hole.FilledAt(NewBin(6, 3)))
+	assert.False(hole.FilledAt(NewBin(8, 0)))
+	assert.False(hole.EmptyAt(NewBin(8, 0)))
+	assert.True(hole.EmptyAt(NewBin(6, 1)))
+}
+
+func TestFind(t *testing.T) {
+	assert := assert.New(t)
+	hole := New()
+	hole.Set(NewBin(4, 0))
+	hole.Reset(NewBin(1, 1))
+	hole.Reset(NewBin(0, 7))
+	assert.Equal(NewBin(0, 2), hole.FindEmpty().BaseLeft())
+}
+
+/*
+func TestAlloc(t *testing.T) {
+	assert := assert.New(t)
+	b := New()
+	b.Set(NewBin(1, 0))
+	b.Set(NewBin(1, 1))
+	b.Reset(NewBin(1, 0))
+	b.Reset(NewBin(1, 1))
+
+  assert.Equal(1, b.CellsNumber())
+}
+*/
+
+func TestCover(t *testing.T) {
+	assert := assert.New(t)
+	b := New()
+	b.Set(NewBin(2, 0))
+	b.Set(NewBin(4, 1))
+
+	assert.Equal(NewBin(4, 1), b.Cover(NewBin(0, 30)))
+	assert.Equal(NewBin(2, 0), b.Cover(NewBin(0, 3)))
+	assert.Equal(NewBin(2, 0), b.Cover(NewBin(2, 0)))
+}
+
+/*
+func TestCopyRange(t *testing.T) {
+	assert := assert.New(t)
+	add := New()
+	data := New()
+	data.Set(NewBin(2, 0))
+	data.Set(NewBin(2, 2))
+	data.Set(NewBin(1, 7))
+	add.Set(NewBin(2, 1))
+	add.Set(NewBin(1, 4))
+	add.Set(NewBin(0, 13))
+	add.Set(NewBin(5, 118))
+	//     binmap_t::copy(data, add, NewBin(3,0));
+	assert.False(data.EmptyAt(NewBin(3, 0)))
+	assert.False(data.FilledAt(NewBin(3, 0)))
+	assert.True(data.EmptyAt(NewBin(2, 0)))
+	assert.True(data.FilledAt(NewBin(2, 1)))
+	assert.True(data.EmptyAt(NewBin(1, 6)))
+	assert.True(data.FilledAt(NewBin(1, 7)))
+}
+*/
+
+func TestSeqLength(t *testing.T) {
+	assert := assert.New(t)
+	b := New()
+	b.Set(NewBin(3, 0))
+	b.Set(NewBin(1, 4))
+	b.Set(NewBin(0, 10))
+	b.Set(NewBin(3, 2))
+	assert.Equal(11, int(b.FindEmpty().BaseOffset()))
+}
+
+func TestEmptyFilled(t *testing.T) {
+	assert := assert.New(t)
+	b := New()
+
+	assert.True(b.EmptyAt(All))
+	b.Set(NewBin(1, 0))
+	b.Set(NewBin(0, 2))
+	b.Set(NewBin(0, 6))
+	b.Set(NewBin(1, 5))
+	b.Set(NewBin(0, 9))
+
+	assert.False(b.EmptyAt(All))
+	assert.True(b.EmptyAt(NewBin(2, 3)))
+	assert.False(b.FilledAt(NewBin(2, 3)))
+	assert.True(b.FilledAt(NewBin(1, 0)))
+	assert.True(b.FilledAt(NewBin(1, 5)))
+	assert.False(b.FilledAt(NewBin(1, 3)))
+
+	b.Set(NewBin(0, 3))
+	b.Set(NewBin(0, 7))
+	b.Set(NewBin(0, 8))
+
+	assert.True(b.FilledAt(NewBin(2, 0)))
+	assert.True(b.FilledAt(NewBin(2, 2)))
+	assert.False(b.FilledAt(NewBin(2, 1)))
+
+	b.Set(NewBin(1, 2))
+	assert.True(b.FilledAt(NewBin(2, 1)))
+}
+
+/*
+func TestFindEmptyStart(t *testing.T) {
+	assert := assert.New(t)
+	hole := New()
+
+	for s := 0; s < 8; s++ {
+		for i := s; i < 8; i++ {
+			hole.Set(NewBin(3, 0))
+			hole.Reset(NewBin(3, uint64(i)))
+			f := hole.FindEmptyAfter(NewBin(0, uint64(s)))
+			assert.Equal(NewBin(0, uint64(i)), f)
+		}
+	}
+}
+*/
+
+func TestFindFilled1(t *testing.T) {
+	assert := assert.New(t)
+	hole := New()
+
+	hole.Set(NewBin(3, 0))
+	hole.Reset(NewBin(0, 0))
+	assert.Equal(NewBin(0, 1), hole.FindFilled())
+}
+
+func TestFindFilled2(t *testing.T) {
+	assert := assert.New(t)
+	hole := New()
+
+	hole.Set(NewBin(3, 0))
+	hole.Reset(NewBin(0, 1))
+	assert.Equal(NewBin(0, 0), hole.FindFilled())
+}
+
+func TestFindFilled3(t *testing.T) {
+	assert := assert.New(t)
+	hole := New()
+
+	hole.Set(NewBin(3, 0))
+	hole.Reset(NewBin(2, 0))
+	assert.Equal(NewBin(0, 4), hole.FindFilled().BaseLeft())
+}
