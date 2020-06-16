21c14153eabfe03746300e7a700b08f73bb49a87
jbpratt78 <jbpratt78@gmail.com>
2020-06-15 22:21:36 -0500

    construct test cases for tests, check for priv len on new cert req

--- pkg/dao/cert_test.go
+++ pkg/dao/cert_test.go
@@ -1,158 +1,140 @@
 package dao
 
 import (
+	"crypto/ed25519"
 	"testing"
 
 	"github.com/MemeLabs/go-ppspp/pkg/pb"
 	"github.com/tj/assert"
 )
 
-func TestVerifyCertificateRequest(t *testing.T) {
-	key := &pb.Key{
-		Type: pb.KeyType_KEY_TYPE_ED25519,
-		Private: []byte(`-----BEGIN OPENSSH PRIVATE KEY-----
-    b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAAAMwAAAAtzc2gtZW
-    QyNTUxOQAAACCFaBtey/Mas1Is8WWjq3QE9Gdgu4HlPZRO/JfmLccS6wAAAJj7aSXh+2kl
-    4QAAAAtzc2gtZWQyNTUxOQAAACCFaBtey/Mas1Is8WWjq3QE9Gdgu4HlPZRO/JfmLccS6w
-    AAAEAQN9fdacoXGr8u6QWshmzGKOJ+VUepzhMEp/MdpkFWH4VoG17L8xqzUizxZaOrdAT0
-    Z2C7geU9lE78l+YtxxLrAAAADmpicHJhdHRAYXV0dW1uAQIDBAUGBw==
-    -----END OPENSSH PRIVATE KEY-----
-    `),
-		Public: []byte(`
-ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIVoG17L8xqzUizxZaOrdAT0Z2C7geU9lE78l+YtxxLr jbpratt@autumn
-    `),
+func generateED25519Key(t *testing.T) *pb.Key {
+	t.Helper()
+	pub, priv, err := ed25519.GenerateKey(nil)
+	if err != nil {
+		t.Fatal(err)
 	}
 
-	tcs := map[string]struct {
-		req *pb.CertificateRequest
-		err error
-	}{
+	return &pb.Key{
+		Type:    pb.KeyType_KEY_TYPE_ED25519,
+		Public:  pub,
+		Private: priv,
+	}
+}
+
+type testcase struct {
+	req  *pb.CertificateRequest
+	cert *pb.Certificate
+	key  *pb.Key
+	err  error
+}
+
+// i don't exactly like this after writing it, though i want to
+// cover all of these areas without managing to repeat myself to much
+func buildTestCases(t *testing.T) (map[string]testcase, error) {
+	t.Helper()
+	key := generateED25519Key(t)
+	successCsr, err := NewCertificateRequest(key, 0)
+	if err != nil {
+		return nil, err
+	}
+
+	invalidLenCsr, err := NewCertificateRequest(key, 0)
+	if err != nil {
+		return nil, err
+	}
+	invalidLenCsr.Key = key.Public[:len(key.Public)-5]
+
+	invalidTypeCsr, err := NewCertificateRequest(key, 0)
+	if err != nil {
+		return nil, err
+	}
+	invalidTypeCsr.KeyType = pb.KeyType_KEY_TYPE_X25519
+
+	tcs := map[string]testcase{
 		"success": {
-			req: &pb.CertificateRequest{
-				Key:      key.Public,
-				KeyType:  key.Type,
-				KeyUsage: 0,
-			},
+			req: successCsr,
+			key: key,
 			err: nil,
 		},
 		"invalid key length": {
-			req: &pb.CertificateRequest{
-				Key:      key.Public[:len(key.Public)-5],
-				KeyType:  key.Type,
-				KeyUsage: 0,
-			},
+			req: invalidLenCsr,
+			key: &pb.Key{Type: key.Type, Private: key.Private[:len(key.Private)-5], Public: key.Public},
 			err: ErrInvalidKeyLength,
 		},
 		"invalid key type (x25519)": {
-			req: &pb.CertificateRequest{
-				Key:      key.Public,
-				KeyType:  pb.KeyType_KEY_TYPE_X25519,
-				KeyUsage: 0,
-			},
+			req: invalidTypeCsr,
+			key: &pb.Key{Type: pb.KeyType_KEY_TYPE_X25519, Private: key.Private, Public: key.Public},
 			err: ErrUnsupportedKeyType,
 		},
 	}
+	return tcs, nil
+}
+
+// Doesn't seem needed as the test cases require this but its here
+func TestNewCertificateRequest(t *testing.T) {
+	tcs, err := buildTestCases(t)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	for scenario, tc := range tcs {
+		t.Run(scenario, func(t *testing.T) {
+			_, err := NewCertificateRequest(tc.key, 0)
+			if tc.err != nil {
+				assert.EqualError(t, err, tc.err.Error())
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
+}
+
+func TestVerifyCertificateRequest(t *testing.T) {
+	tcs, err := buildTestCases(t)
+	if err != nil {
+		t.Fatal(err)
+	}
+
 	for scenario, tc := range tcs {
 		t.Run(scenario, func(t *testing.T) {
 			err := VerifyCertificateRequest(tc.req, 0)
-			// if this test case should error, check
 			if tc.err != nil {
 				assert.EqualError(t, err, tc.err.Error())
+			} else {
+				assert.NoError(t, err)
 			}
 		})
 	}
 }
 
 func TestSignCertificateRequest(t *testing.T) {
-	key := &pb.Key{
-		Type: pb.KeyType_KEY_TYPE_ED25519,
-		Private: []byte(`-----BEGIN OPENSSH PRIVATE KEY-----
-    b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAAAMwAAAAtzc2gtZW
-    QyNTUxOQAAACCFaBtey/Mas1Is8WWjq3QE9Gdgu4HlPZRO/JfmLccS6wAAAJj7aSXh+2kl
-    4QAAAAtzc2gtZWQyNTUxOQAAACCFaBtey/Mas1Is8WWjq3QE9Gdgu4HlPZRO/JfmLccS6w
-    AAAEAQN9fdacoXGr8u6QWshmzGKOJ+VUepzhMEp/MdpkFWH4VoG17L8xqzUizxZaOrdAT0
-    Z2C7geU9lE78l+YtxxLrAAAADmpicHJhdHRAYXV0dW1uAQIDBAUGBw==
-    -----END OPENSSH PRIVATE KEY-----
-    `),
-		Public: []byte(`
-ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIVoG17L8xqzUizxZaOrdAT0Z2C7geU9lE78l+YtxxLr jbpratt@autumn
-    `),
+	tcs, err := buildTestCases(t)
+	if err != nil {
+		t.Fatal(err)
 	}
 
-	tcs := map[string]struct {
-		req *pb.CertificateRequest
-		key *pb.Key
-		err error
-	}{
-		"success": {
-			key: key,
-			req: &pb.CertificateRequest{
-				Key:      key.Public,
-				KeyType:  key.Type,
-				KeyUsage: 0,
-			},
-			err: nil,
-		},
-		"invalid key length": {
-			key: &pb.Key{Type: key.Type, Private: key.Private[:len(key.Private)-5], Public: key.Public},
-			req: &pb.CertificateRequest{
-				Key:      key.Public,
-				KeyType:  key.Type,
-				KeyUsage: 0,
-			},
-			err: ErrInvalidKeyLength,
-		},
-		"invalid key type (x25519)": {
-			key: &pb.Key{Type: pb.KeyType_KEY_TYPE_X25519, Private: key.Private, Public: key.Public},
-			req: &pb.CertificateRequest{
-				Key:      key.Public,
-				KeyType:  key.Type,
-				KeyUsage: 0,
-			},
-			err: ErrUnsupportedKeyType,
-		},
-	}
 	for scenario, tc := range tcs {
 		t.Run(scenario, func(t *testing.T) {
 			_, err := SignCertificateRequest(tc.req, defaultCertTTL, tc.key)
 			if tc.err != nil {
 				assert.EqualError(t, err, tc.err.Error())
+			} else {
+				assert.NoError(t, err)
 			}
+
+			// TODO: inspect the cert
 		})
 	}
 }
 
 /*
 func TestVerifyCertificate(t *testing.T) {
-	key := &pb.Key{
-		Type: pb.KeyType_KEY_TYPE_ED25519,
-		Private: []byte(`-----BEGIN OPENSSH PRIVATE KEY-----
-    b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAAAMwAAAAtzc2gtZW
-    QyNTUxOQAAACCFaBtey/Mas1Is8WWjq3QE9Gdgu4HlPZRO/JfmLccS6wAAAJj7aSXh+2kl
-    4QAAAAtzc2gtZWQyNTUxOQAAACCFaBtey/Mas1Is8WWjq3QE9Gdgu4HlPZRO/JfmLccS6w
-    AAAEAQN9fdacoXGr8u6QWshmzGKOJ+VUepzhMEp/MdpkFWH4VoG17L8xqzUizxZaOrdAT0
-    Z2C7geU9lE78l+YtxxLrAAAADmpicHJhdHRAYXV0dW1uAQIDBAUGBw==
-    -----END OPENSSH PRIVATE KEY-----
-    `),
-		Public: []byte(`
-ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIVoG17L8xqzUizxZaOrdAT0Z2C7geU9lE78l+YtxxLr jbpratt@autumn
-    `),
+	tcs, err := buildTestCases(t)
+	if err != nil {
+		t.Fatal(err)
 	}
 
-	tcs := map[string]struct {
-		cert *pb.Certificate
-		err  error
-	}{
-		"success": {
-			err: nil,
-		},
-		"invalid key length": {
-			err: ErrInvalidKeyLength,
-		},
-		"invalid key type (x25519)": {
-			err: ErrUnsupportedKeyType,
-		},
-	}
 	for scenario, tc := range tcs {
 		t.Run(scenario, func(t *testing.T) {
 			if err := VerifyCertificate(tc.cert); err != nil {
