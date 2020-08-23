1ac60c165f2749229d36c9c1c609b0a96c108e72
Slugalisk <slugalisk@gmail.com>
2020-08-22 21:07:11 -0700

    parallelize sku loading in infra cli

--- infra/hack/kubernetes/README.md
+++ infra/hack/kubernetes/README.md
@@ -16,3 +16,18 @@ jb init
 kubectl create -f manifests/setup
 kubectl create -f manifests
 ```
+
+#### dashboards
+##### kubernetes
+```
+kube proxy
+```
+http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/
+##### grafana
+```
+kubectl --namespace monitoring port-forward svc/grafana 3000
+```
+##### prometheus
+```
+kubectl --namespace monitoring port-forward svc/prometheus-k8s 9090
+```
--- infra/internal/cmd/skus.go
+++ infra/internal/cmd/skus.go
@@ -9,9 +9,11 @@ import (
 	"github.com/MemeLabs/go-ppspp/infra/pkg/node"
 	"github.com/olekukonko/tablewriter"
 	"github.com/spf13/cobra"
+	"golang.org/x/sync/errgroup"
 )
 
 func init() {
+	skusCmd.PersistentFlags().StringP("region", "r", "", "provider region")
 	rootCmd.AddCommand(skusCmd)
 }
 
@@ -41,6 +43,11 @@ var skusCmd = &cobra.Command{
 		}
 		var data [][]string
 
+		region, _ := cmd.PersistentFlags().GetString("region")
+		req := &node.SKUsRequest{
+			Region: region,
+		}
+
 		if len(args) == 1 {
 			provider := args[0]
 			driver, ok := backend.NodeDrivers[provider]
@@ -48,7 +55,7 @@ var skusCmd = &cobra.Command{
 				return fmt.Errorf("Unsupported provider: %s", provider)
 			}
 
-			rows, err := formatProviderSKUs(cmd.Context(), driver)
+			rows, err := formatProviderSKUs(cmd.Context(), driver, req)
 			if err != nil {
 				return err
 			}
@@ -56,12 +63,21 @@ var skusCmd = &cobra.Command{
 		} else {
 			header = append([]string{"Provider"}, header...)
 			alignment = append([]int{tablewriter.ALIGN_DEFAULT}, alignment...)
+
+			eg, ctx := errgroup.WithContext(cmd.Context())
 			for _, driver := range backend.NodeDrivers {
-				rows, err := formatProviderSKUs(cmd.Context(), driver)
-				if err != nil {
-					return err
-				}
-				data = append(data, prependProviderColumn(rows, driver)...)
+				driver := driver
+				eg.Go(func() error {
+					rows, err := formatProviderSKUs(ctx, driver, req)
+					if err != nil {
+						return err
+					}
+					data = append(data, prependProviderColumn(rows, driver)...)
+					return nil
+				})
+			}
+			if err := eg.Wait(); err != nil {
+				return err
 			}
 		}
 
@@ -75,8 +91,8 @@ var skusCmd = &cobra.Command{
 	},
 }
 
-func formatProviderSKUs(ctx context.Context, driver node.Driver) ([][]string, error) {
-	skus, err := driver.SKUs(ctx, &node.SKUsRequest{Region: "us-lax1"})
+func formatProviderSKUs(ctx context.Context, driver node.Driver, req *node.SKUsRequest) ([][]string, error) {
+	skus, err := driver.SKUs(ctx, req)
 	if err != nil {
 		return nil, fmt.Errorf("Loading SKUs failed: %w", err)
 	}
