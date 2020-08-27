1f4d395d20eda9fb9142c67230ecc6dbaafa3888
jbpratt78 <jbpratt78@gmail.com>
2020-08-26 20:05:40 -0500

    fix build

--- android/app/src/main/java/gg/strims/ppspp/AndroidBridge.kt
+++ android/app/src/main/java/gg/strims/ppspp/AndroidBridge.kt
@@ -3,16 +3,15 @@ package gg.strims.ppspp
 import bridge.AndroidSide
 import bridge.Bridge
 import bridge.GoSide
-import java.lang.Exception
 
 class AndroidBridge(location: String) : AndroidSide {
-    public var onData: (_: ByteArray?) -> Unit = {_: ByteArray? -> }
-    public var g : GoSide = Bridge.newGoSide(this, location)
+    public var onData: (_: ByteArray?) -> Unit = { _: ByteArray? -> }
+    public var g: GoSide = Bridge.newGoSide(this, location)
     public var error: Throwable? = null
 
     fun write(b: ByteArray) {
         this.error?.let { throw it }
-        g.write(b)!!
+        g.write(b)
     }
 
     override fun emitError(msg: String?) {
