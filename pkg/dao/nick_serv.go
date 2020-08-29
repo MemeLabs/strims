95d1eb6907f52868c9a86b51f28146cca3bf21a2
SoMuchForSubtlety <s0muchfrsubtlety@gmail.com>
2020-08-30 00:38:37 +0200

    fixes

--- pkg/dao/nick_serv.go
+++ pkg/dao/nick_serv.go
@@ -7,38 +7,50 @@ import (
 	"github.com/MemeLabs/go-ppspp/pkg/pb"
 )
 
-const nickRecordPrefix = "nickRecord:"
+const nickRecordPrefix = "nickservNick:"
 
-func prefixNickRecord(id uint64) string {
-	return nickRecordPrefix + strconv.FormatUint(id, 10)
+func prefixNickRecord(nickservID, recordID uint64) string {
+	return prefix(nickservID) + strconv.FormatUint(recordID, 10)
+}
+
+func prefix(nickservID uint64) string {
+	return nickRecordPrefix + strconv.FormatUint(nickservID, 10) + ":"
 }
 
 // UpsertNickRecord inserts or updates the record in the provided kv store
-func UpsertNickRecord(s kv.RWStore, v *pb.NickServRecord) error {
+func UpsertNickRecord(s kv.RWStore, v *pb.NickServRecord, nickservID uint64) error {
 	return s.Update(func(tx kv.RWTx) error {
-		return tx.Put(prefixNickRecord(v.Id), v)
+		return tx.Put(prefixNickRecord(nickservID, v.Id), v)
 	})
 }
 
 // DeleteNickRecord deletes the record from the provided kv store
-func DeleteNickRecord(s kv.RWStore, id uint64) error {
+func DeleteNickRecord(s kv.RWStore, nickservID, recordID uint64) error {
 	return s.Update(func(tx kv.RWTx) error {
-		return tx.Delete(prefixNickRecord(id))
+		return tx.Delete(prefixNickRecord(nickservID, recordID))
 	})
 }
 
 // GetNickRecord retrieves the record with the specified id from the provided kv store
-func GetNickRecord(s kv.Store, id uint64) (v *pb.NickServRecord, err error) {
+func GetNickRecord(s kv.Store, nickservID, recordID uint64) (v *pb.NickServRecord, err error) {
 	err = s.View(func(tx kv.Tx) error {
-		return tx.Get(prefixNickRecord(id), v)
+		return tx.Get(prefixNickRecord(nickservID, recordID), v)
 	})
 	return v, err
 }
 
 // GetAllNickRecords gets all records from the provided kv store
-func GetAllNickRecords(s kv.Store) (v []*pb.NickServRecord, err error) {
+func GetAllNickRecords(s kv.Store, nickservID uint64) (v []*pb.NickServRecord, err error) {
+	v = []*pb.NickServRecord{}
 	err = s.View(func(tx kv.Tx) error {
-		return tx.ScanPrefix(nickRecordPrefix, v)
+		return tx.ScanPrefix(prefix(nickservID), &v)
+	})
+	return v, err
+}
+
+func GetNickservConfig(s kv.Store, nickservID uint64) (v *pb.ServerConfig, err error) {
+	err = s.View(func(tx kv.Tx) error {
+		return tx.Get(prefix(nickservID), v)
 	})
 	return v, err
 }
