ae07b2d07c60813202d99106ed03cc6946a36efa
jbpratt <jbpratt78@gmail.com>
2021-02-08 19:25:28 -0600

    mobile: gobridge cache and rework wire gen (#84)
    closes #81
    updates #76

--- hack/sanitize-proto.sh
+++ hack/sanitize-proto.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-SCHEMA_DIR="schema"
-INPUT_DIR="$SCHEMA_DIR/sanitized"
-rm -rf $INPUT_DIR
-mkdir -p $INPUT_DIR
-rm $INPUT_DIR/*.proto || echo "nothing to clean"
-hack/remove-services.sh $INPUT_DIR "$(find $SCHEMA_DIR -type f)"
+grep -lr --include="*.kt" "com.squareup.wire.Grpc*" | xargs rm
